374c1b1fefd39d1d6de5d749f9806216e7b3e193
Sebastian Messmer <messmer@fb.com>
2019-09-10 16:25:55 -0700

    Register ATen ops with c10
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/23667
    
    Changes in this PR:
    - For each operator with use_c10_dispatcher: True, additionally generate a c10 registration line in TypeDefault.cpp, CPUType.cpp, and other backend files.
    - This doesn't change globalATenDispatch yet, the c10 registration is purely additional and the operator calling path doesn't change. A diff further up the stack will change these things.
    - Enable the use_c10_dispatcher: True flag for about ~70% of operators
    - This also changes the c10->jit operator export because ATen ops are already exported to JIT directly and we don't want to export the registered c10 ops because they would clash
    - For this, we need a way to recognize if a certain operator is already moved from ATen to c10, this is done by generating a OpsAlreadyMovedToC10.cpp file with the list. A diff further up in the stack will also need this file to make sure we don't break the backend extension API for these ops.
    
    
    Reasons for some ops to be excluded (i.e. not have the `use_c10_dispatcher` flag set to true):
    - `Tensor?(a!)` (i.e. optional tensor with annotations) not supported in c++ function schema parser yet
    - `-> void` in native_functions.yaml vs `-> ()` expected by function schema parser
    - out functions have different argument order in C++ as in the jit schema
    - `Tensor?` (i.e. optional tensor) doesn't work nicely with undefined tensor sometimes being undefined tensor and sometimes being None.
    - fixed-size arrays like `int[3]` not supported in c10 yet
    
    These will be fixed in separate diffs and then the exclusion tag will be removed.
    
    ghstack-source-id: 89852079
    
    Differential Revision: [D16603131](https://our.internmc.facebook.com/intern/diff/D16603131/)

--- aten/src/ATen/templates/TypeDerived.cpp
+++ aten/src/ATen/templates/TypeDerived.cpp
@@ -21,12 +21,14 @@ $storage_tensor_headers
 #include <c10/core/UndefinedTensorImpl.h>
 #include <c10/util/Optional.h>
 #include <ATen/core/ATenDispatch.h>
+#include <ATen/core/op_registration/op_registration.h>
 
 #include <cstddef>
 #include <functional>
 #include <memory>
 #include <utility>
 
+#include <ATen/core/op_registration/op_registration.h>
 #include <ATen/Config.h>
 $extra_cuda_headers
 $legacy_th_headers
@@ -42,6 +44,13 @@ Tensor * ${Type}::add(Tensor & a, Tensor & b) {
 
 ${type_derived_method_definitions}
 
+namespace {
+
 static auto& registerer = globalATenDispatch()
   ${function_registrations};
+
+static auto c10_registerer = torch::RegisterOperators()
+  ${c10_function_registrations};
+
+}
 }
