2ec7762a1d689ac4b667700afafc69d6960001f3
Supriya Rao <supriyar@fb.com>
2019-09-10 18:04:11 -0700

    [quantization] Add Runtime flag for quantized backend.
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/25680
    
    Add a runtime flag to choose between FBGEMM and QNNPACK when compiled with both.
    
    The flag can be set by using torch.backends.quantized.engine = torch.fbgemm/torch.qnnpack or ctx::setPreferredQuantizedEngine(at::QEngine)
    ghstack-source-id: 89863090
    
    Differential Revision: [D17198233](https://our.internmc.facebook.com/intern/diff/D17198233/)

--- aten/src/ATen/Context.h
+++ aten/src/ATen/Context.h
@@ -11,6 +11,7 @@
 #include <ATen/detail/HIPHooksInterface.h>
 #include <c10/util/Exception.h>
 #include <c10/core/impl/DeviceGuardImplInterface.h>
+#include <c10/core/QEngine.h>
 
 #include <memory>
 #include <mutex>
@@ -103,6 +104,8 @@ class CAFFE2_API Context {
   void setBenchmarkCuDNN(bool);
   bool deterministicCuDNN() const;
   void setDeterministicCuDNN(bool);
+  at::QEngine preferredQuantizedEngine() const;
+  void setPreferredQuantizedEngine(at::QEngine e);
 private:
   void initCUDAIfNeeded(DeviceType p) {
     if (p == DeviceType::CUDA) {
@@ -119,6 +122,7 @@ private:
   bool enabled_cudnn = true;
   bool deterministic_cudnn = false;
   bool benchmark_cudnn = false;
+  at::QEngine quantized_engine = at::QEngine::FBGEMM;
   std::unique_ptr<THCState, void(*)(THCState*)> thc_state;
   std::unique_ptr<THHState, void(*)(THHState*)> thh_state;
 };
